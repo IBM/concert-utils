07f22fff0ebc5bb6695a0e8282c4f6df9f47caea
Jose Ortiz <jortiz@us.ibm.com>
2024-07-16 17:11:06 -0400

    renaming constants

--- helpers/image-scan-stage-gen-cyclondx-sbom.sh
+++ helpers/image-scan-stage-gen-cyclondx-sbom.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+source constants.variables
+
 SCAN_COMMAND="image-scan --images ${IMAGE_NAME}:${IMAGE_TAG}"
 docker run -it --rm -u $(id -u):$(id -g) -v ${SRC_PATH}:/concert-sample-src -v ${OUTPUTDIR}:/toolkit-data ${CONCERT_TOOLKIT_IMAGE} bash -c "${SCAN_COMMAND}"
