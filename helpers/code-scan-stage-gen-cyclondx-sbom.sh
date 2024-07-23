a996202e821da09255b4e2b9cdcf0cccbeed8a2d
Jose Ortiz <jortiz@us.ibm.com>
2024-07-23 11:20:16 -0400

    Update code-scan-stage-gen-cyclondx-sbom.sh

--- helpers/code-scan-stage-gen-cyclondx-sbom.sh
+++ helpers/code-scan-stage-gen-cyclondx-sbom.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SCRIPT_DIR}/constants.variables
 
 
-OUTPUT_FILENAME="$REPO_NAME-$BUILD_NUMBER-cyclonedx-sbom.json"
+OUTPUT_FILENAME="${REPO_NAME}-cyclonedx-sbom-${BUILD_NUMBER}.json"
 
 CODE_SCAN_COMMAND="code-scan --src /concert-sample --output-file ${OUTPUT_FILENAME}"
 docker run -it --rm -u $(id -u):$(id -g) -v ${SRC_PATH}:/concert-sample -v ${OUTPUTDIR}:/toolkit-data ${CONCERT_TOOLKIT_IMAGE} bash -c "${CODE_SCAN_COMMAND}"
