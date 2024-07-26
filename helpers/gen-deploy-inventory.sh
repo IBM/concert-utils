9dc7f22e4dddeef38809fcf9c16905814e16b63e
Jose Ortiz <jortiz@us.ibm.com>
2024-07-26 12:25:57 -0400

    minor enhancements

--- helpers/gen-deploy-inventory.sh
+++ helpers/gen-deploy-inventory.sh
@@ -2,6 +2,33 @@
 ### WORK IN PROGESS
 
 
+usage() {
+    echo "Usage: $(basename $0) --outputfile <filename for the generated json>"
+    echo "Example: $(basename $0) --outputfile deploy-inventory.json"
+    exit 1
+}
+
+if [ "$#" -eq 0 ]; then
+    usage
+fi
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --output_file)
+            output_file="$2"
+            [ -z "$output_file" ] && { echo "Error: --outputfile <filename for the generated json> is required."; usage; }
+            shift 2
+            ;;
+        --help)
+            usage
+            ;;
+        *)
+            echo "Unknown parameter passed: $1"
+            usage
+            ;;
+    esac
+done
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SCRIPT_DIR}/constants.variables
 
@@ -11,7 +38,9 @@ export TIMESTAMP_UTC=$(date -u "+%Y-%m-%dT%H:%M:%SZ")
 export IMAGE_PURL=$IMAGE_NAME:$IMAGE_TAG
 export IMAGE_URI="${IMAGE_PURL}@${IMAGE_DIGEST}"
 
+
 #echo "generating build inventory config ${OUTPUTDIR}/${outfile_name} "
+export OUTPUT_FILENAME=$output_file
 CONCERT_DEF_CONFIG_FILE=deploy-${COMPONENT_NAME}-${BUILD_NUMBER}-config.yaml
 envsubst < ${SCRIPT_DIR}/${TEMPLATE_PATH}/deploy-sbom-values.yaml.template > ${OUTPUTDIR}/${CONCERT_DEF_CONFIG_FILE}
 
