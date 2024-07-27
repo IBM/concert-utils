b8a95dc5645517d0484d2c4a31bb06fd61785db5
Jose Ortiz <jortiz@us.ibm.com>
2024-07-27 10:45:22 -0400

    Update gen-deploy-inventory.sh

--- helpers/gen-deploy-inventory.sh
+++ helpers/gen-deploy-inventory.sh
@@ -32,18 +32,6 @@ done
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source ${SCRIPT_DIR}/constants.variables
 
-# generate deploy file
-#
-export TIMESTAMP_UTC=$(date -u "+%Y-%m-%dT%H:%M:%SZ")
-export IMAGE_PURL=$IMAGE_NAME:$IMAGE_TAG
-export IMAGE_URI="${IMAGE_PURL}@${IMAGE_DIGEST}"
-
-
-#echo "generating build inventory config ${OUTPUTDIR}/${outfile_name} "
-export OUTPUT_FILENAME=$outputfile
-CONCERT_DEF_CONFIG_FILE=deploy-${COMPONENT_NAME}-${BUILD_NUMBER}-config.yaml
-envsubst < ${SCRIPT_DIR}/${TEMPLATE_PATH}/deploy-sbom-values.yaml.template > ${OUTPUTDIR}/${CONCERT_DEF_CONFIG_FILE}
-
 TOOLKIT_COMMAND="deploy-sbom --deploy-config /toolkit-data/${CONCERT_DEF_CONFIG_FILE}"
 docker run -it --rm -u $(id -u):$(id -g) -v ${OUTPUTDIR}:/toolkit-data ${CONCERT_TOOLKIT_IMAGE} bash -c "${TOOLKIT_COMMAND}"
 
